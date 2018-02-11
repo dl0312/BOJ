97b1b6d864eca5b6152753d0969c25bee626cfc1
GeonLee <leegun2003@gmail.com>
2018-02-11 19:26:48 +0900

    ing
    ing

--- ing/Main.java
+++ ing/Main.java
@@ -1,238 +1,263 @@
 
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.Scanner;
 
-//	BOJ - 14948
+//	BOJ - 3190
 //	Algorithm - ?
-//	2017 Sogang Programming Contest - Champion F¹ø
-
-/*
- 11122
-2>3>12>1>3>2
-0>0>0>1>0>0
-43
-
-4 3
-2 5 2
-3 4 3
-12 1 3
-1 3 2
- */
+//	Samsung SW #3
 
 public class Main {
-	private static int n,m;
 	
-	public static int[] update(int [] the_list, int n, int [] result) {
-		int []templist = new int [the_list.length-n];
-		System.arraycopy(the_list, n, templist, 0, the_list.length-n);
-		Arrays.sort(templist);
-		System.arraycopy(templist, 0, the_list, n, templist.length);
-		System.arraycopy(the_list, 0, result, 0, the_list.length);
-		return the_list;
-	}
+	static int n;
+	static int k;
+	static long l;
 	
-	public static int factorial(int n) {
-		if (n <= 1)
-			return n;
-		else 
-			return factorial(n-1) * n;
-	}
-	
-	public static int [][] permute(int [] source){
-		int [] reverse_source = new int [source.length];
-		for(int i=0;i<source.length;i++) {
-			reverse_source[source.length-i-1] = source[i];
+	private static class Line{
+		Pos str;
+		Pos end;
+		
+		public Line(Pos str, Pos end) {
+			this.str = str;
+			this.end = end;
 		}
-		int [][] permute_ary = new int [permute_num(n-1,m-1)][n+m-2];
-		int [] result = new int [source.length];
-		int cnt =0;
-		System.arraycopy(source, 0, result, 0, source.length);
-		while( !Arrays.equals(source, reverse_source) && cnt < factorial(source.length)) {
-			System.arraycopy(source, 0, permute_ary[cnt], 0, source.length);
-			cnt++;
-			for(int i=source.length-1;i>0;i--) {
-				if(source[i]>source[i-1]) {
-					for(int j=i;j<source.length;j++) {
-						if(source[i-1]>=source[j]) {
-							int tmp = source[i-1];
-							source[i-1] = source[j-1];
-							source[j-1] = tmp;
-							source = update(source,i,result);
-							break;
-						}
-						else if(j==source.length-1) {
-							if(source[i-1]!=source[j]) {
-								int tmp = source[i-1];
-								source[i-1] = source[j];
-								source[j] = tmp;
-								source = update(source,i,result);
-								break;
+		
+		public boolean is_pos_contain(Pos pos) {
+			if(pos.x == str.x && pos.x == end.x) {
+				if(pos.y>=this.str.y && pos.y<=this.end.y || pos.y<=this.str.y && pos.y>=this.end.y) {
+					return true;
+				}
+			}
+			else if(pos.y == this.str.y && pos.y == this.end.y) {
+				if(pos.x>=this.str.x && pos.x<=this.end.x || pos.x<=this.str.x && pos.x>=this.end.x) {
+					return true;
+				}
+			}
+			return false;
+		}
+		
+		public boolean is_line_cross(Line line) {
+			
+		}
+	}
+	
+	private static class Snake{
+		int second = 0;
+		boolean finished = false;
+		Pos head;
+		int dir=0; // 0:east, 1:south, 2:west, 3:north
+		int length=1;
+		boolean crash = false;
+		
+//		LinkedList<Curve_Pos> curve_list = new LinkedList<Curve_Pos>();
+		LinkedList<Integer> sep_length = new LinkedList<Integer>();
+		LinkedList<Pos> trace = new LinkedList<Pos>();
+		
+		public Snake() {
+			this.head = new Pos(0,0);
+			this.trace.add(new Pos(0,0));
+		}
+		
+		public void curve(char curve) {
+			/*
+			this.snake.curve_list.add(new Curve_Pos(new Pos(this.snake.head.x,this.snake.head.y),(this.snake.dir+2)%4));
+			*/
+			if(curve=='L') {
+				this.dir = (this.dir+3)%4;
+			}
+			else if(curve=='R'){
+				this.dir = (this.dir+1)%4;
+			}
+			this.trace.add(new Pos(this.head.x,this.head.y));
+			/*s
+			this.snake.sep_length.add(0);
+			*/
+		}
+		
+		public void print() {
+			System.out.println("head: " + this.head.x  +", " + this.head.y);
+			System.out.println("Second " + this.second);
+		}
+		
+		public void move(long distance) {
+			while(distance!=0) {
+//				this.print();
+				if(this.dir==0) {
+					this.head.y+=1;
+				}
+				else if(this.dir==1) {
+					this.head.x+=1;
+				}
+				else if(this.dir==2) {
+					this.head.y-=1;
+				}
+				else if(this.dir==3) {
+					this.head.x-=1;
+				}
+				for(int i=0;i<trace.size()&&trace.size()>3;i++) {
+					if(trace.get(i).x==this.head.x){
+						if(i==0) {
+							if(trace.get(i+1).x==this.head.x) {
+								if((trace.get(i+1).y >= this.head.y && trace.get(i).y <= this.head.y) || (trace.get(i+1).y <= this.head.y && trace.get(i).y >= this.head.y)){
+									crash = true;
+								}
 							}
-							else {
-								for(int k=j;k>0;k--) {
-									if(source[i-1]!=source[k]) {
-										int tmp = source[i-1];
-										source[i-1] = source[k];
-										source[k] = tmp;
-										source = update(source,i,result);
-										break;
-									}
+						}
+						else if(i==trace.size()-1) {
+							if(trace.get(i-1).x==this.head.x) {
+								if((trace.get(i-1).y >= this.head.y && trace.get(i).y <= this.head.y) || (trace.get(i+1).y <= this.head.y && trace.get(i).y >= this.head.y)){
+									crash = true;
+								}
+							}
+						}
+						else {
+							if(trace.get(i+1).x==this.head.x) {
+								if((trace.get(i+1).y >= this.head.y && trace.get(i).y <= this.head.y) || (trace.get(i+1).y <= this.head.y && trace.get(i).y >= this.head.y)){
+									crash = true;
+								}
+							}
+							if(trace.get(i-1).x==this.head.x) {
+								if((trace.get(i-1).y >= this.head.y && trace.get(i).y <= this.head.y) || (trace.get(i+1).y <= this.head.y && trace.get(i).y >= this.head.y)){
+									crash = true;
 								}
 							}
 						}
 					}
-					break;
-				}
-			}
-		}
-		System.arraycopy(source, 0, permute_ary[cnt], 0, source.length);
-		cnt++;
-		return permute_ary;
-	}
-	
-	public static int permute_num(int n,int m) {
-		return (factorial(n+m)/(factorial(n)*factorial(m)));
-	}
-	
-	private static class Path{
-		int [] path;
-		boolean [] curve;
-		int max_pivot;
-		int max_lv=0;
-		
-		public Path(int [] path) {
-			this.path = path;
-		}
-		public Path() {
-			this.path = new int [n+m-1];
-			this.curve = new boolean [n+m-1];
-			for(int i=0;i<n+m-1;i++) {
-				this.path[i] = 0;
-				this.curve[i] = false;
-			}
-		}
-		public int max_lv() {
-			for(int i=0;i<n+m-1;i++) {
-				if(i!=max_pivot) {
-					if(max_lv<this.path[i]) {
-						max_lv=this.path[i];
+					else if(trace.get(i).y==this.head.y) {
+						if(i==0) {
+							if(trace.get(i+1).y==this.head.y) {
+								if((trace.get(i+1).x >= this.head.x && trace.get(i).x <= this.head.x) || (trace.get(i+1).x <= this.head.x && trace.get(i).x >= this.head.x)){
+									crash = true;
+								}
+							}
+						}
+						else if(i==trace.size()-1) {
+							if(trace.get(i-1).y==this.head.y) {
+								if((trace.get(i-1).x >= this.head.x && trace.get(i).x <= this.head.x) || (trace.get(i+1).x <= this.head.x && trace.get(i).x >= this.head.x)){
+									crash = true;
+								}
+							}
+						}
+						else {
+							if(trace.get(i+1).y==this.head.y) {
+								if((trace.get(i+1).x >= this.head.x && trace.get(i).x <= this.head.x) || (trace.get(i+1).x <= this.head.x && trace.get(i).x >= this.head.x)){
+									crash = true;
+								}
+							}
+							if(trace.get(i-1).y==this.head.y) {
+								if((trace.get(i-1).x >= this.head.x && trace.get(i).x <= this.head.x) || (trace.get(i+1).x <= this.head.x && trace.get(i).x >= this.head.x)){
+									crash = true;
+								}
+							}
+						}
 					}
 				}
+				if(Math.abs(this.head.x)>l || Math.abs(this.head.y)>l) {
+					distance=1;
+					this.finished = true;
+				}
+				else if(crash==true) {
+					distance=1;
+					this.finished = true;
+				}
+				else {
+					/*
+					this.snake.sep_length.set(0, this.snake.sep_length.get(0)-1);	// Ã³À½²¨ -1
+					this.snake.sep_length.set(this.snake.sep_length.size()-1, this.snake.sep_length.get(this.snake.sep_length.size()-1)+1);
+					if(this.snake.curve_list.isEmpty()==false) {
+						long tmp_x = this.snake.curve_list.get(0).curve_pos.x;
+						long tmp_y = this.snake.curve_list.get(0).curve_pos.y;
+						if(this.snake.curve_list.get(0).curve_dir==0) {
+							tmp_y+=this.snake.sep_length.get(0);
+						}
+						else if(this.snake.curve_list.get(0).curve_dir==1) {
+							tmp_x+=this.snake.sep_length.get(0);
+						}
+						else if(this.snake.curve_list.get(0).curve_dir==2) {
+							tmp_y-=this.snake.sep_length.get(0);
+						}
+						else if(this.snake.curve_list.get(0).curve_dir==3) {
+							tmp_x-=this.snake.sep_length.get(0);
+						}
+					}
+					else if(this.snake.curve_list.isEmpty()==true) {
+						long tmp_x = this.snake.head.x;
+						long tmp_y = this.snake.head.y;
+						if(this.snake.dir==2) {
+							tmp_y+=this.snake.sep_length.get(0);
+						}
+						else if(this.snake.dir==3) {
+							tmp_x+=this.snake.sep_length.get(0);
+						}
+						else if(this.snake.dir==0) {
+							tmp_y-=this.snake.sep_length.get(0);
+						}
+						else if(this.snake.dir==1) {
+							tmp_x-=this.snake.sep_length.get(0);
+						}
+					}
+					*/
+				}
+				distance--;
+				this.second++;
 			}
-			return this.max_lv;
+		}
+	}
+	/*
+	private static class Curve_Pos{
+		Pos curve_pos;
+		int curve_dir;
+		
+		public Curve_Pos(Pos curve_pos, int curve_dir) {
+			this.curve_pos = curve_pos;
+			this.curve_dir = curve_dir;
+		}
+	}
+	*/
+	private static class Pos{
+		long x;
+		long y;
+		
+		public Pos(long x, long y) {
+			this.x = x;
+			this.y = y;
+		}
+
+		public long getX() {
+			return x;
+		}
+
+		public void setX(long x) {
+			this.x = x;
+		}
+
+		public long getY() {
+			return y;
+		}
+
+		public void setY(long y) {
+			this.y = y;
 		}
 		
-		public void print() {
-			this.max_lv();
-			System.out.println(Arrays.toString(this.path));
-			System.out.println(Arrays.toString(this.curve));
-			System.out.println("max_pivot: " + this.max_pivot);
-			System.out.println("max_lv: " + this.max_lv);
-		}
 	}
 	
-	private static class Map{
-		int [][] map;
-		public Map(Scanner scn) {
-			this.map = new int [n][m];
-			for(int i=0;i<n;i++) {
-				for(int j=0;j<m;j++) {
-					this.map[i][j] = scn.nextInt();
-				}
-			}
-		}
-		
-		public void print() {
-			for(int i=0;i<n;i++) {
-				System.out.println(map[i]);
-			}
-		}
-		
-		public Path path(int [] permute_ary) {
-			Path path = new Path();
-			int pivot=0;
-			int x_pivot=0;
-			int y_pivot=0;
-			int max_num=0;
-			path.path[pivot] = map[x_pivot][y_pivot];
-			pivot++;
-			if(permute_ary[0]==1) {
-				x_pivot++;
-				path.path[pivot] = this.map[x_pivot][y_pivot];
-			}
-			else if(permute_ary[0]==2) {
-				y_pivot++;
-				path.path[pivot] = this.map[x_pivot][y_pivot];
-			}
-			pivot++;
-			for(int i=1;i<n+m-3;i++) {
-				if(permute_ary[i]!=permute_ary[i+1]) {
-					path.curve[i+1] = true;
-				}
-				if(permute_ary[i]==1) {
-					x_pivot++;
-					path.path[pivot] = this.map[x_pivot][y_pivot];
-					if(max_num<path.path[pivot] && path.curve[i]==false) {
-						max_num = path.path[pivot];
-						path.max_pivot = pivot;
-					}
-				}
-				else if(permute_ary[i]==2) {
-					y_pivot++;
-					path.path[pivot] = this.map[x_pivot][y_pivot];
-					if(max_num<path.path[pivot] && path.curve[i]==false) {
-						max_num = path.path[pivot];
-						path.max_pivot = pivot;
-					}
-				}
-				pivot++;
-			}
-			if(permute_ary[n+m-3]==1) {
-				x_pivot++;
-				path.path[pivot] = this.map[x_pivot][y_pivot];
-			}
-			else if(permute_ary[n+m-3]==2) {
-				y_pivot++;
-				path.path[pivot] = this.map[x_pivot][y_pivot];
-			}
-			pivot++;
-			
-			return path;
-		}
-		/*
-		public int exit(int [][] permute_ary) {
-			Path path = new Path();
-			int pivot = 0;
-			path.path[0] = this.map[0][0];
-			
-		}
-		*/
-	}
+	
 	
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
+		l = scn.nextLong();
 		n = scn.nextInt();
-		m = scn.nextInt();
-		Map map = new Map(scn);
-		int min_lv=0;
-		int [] ary = new int [n+m-2];
-		int pivot = 0;
-		for(;pivot<n-1;pivot++) {	// down : 1
-			ary[pivot] = 1;
+		Snake snake = new Snake();
+		for(int i=0;i<n&&snake.finished==false;i++) {
+			snake.move(scn.nextInt());
+			snake.curve(scn.next().charAt(0));
 		}
-		for(;pivot<n+m-2;pivot++) {
-			ary[pivot] = 2;
+		if(snake.finished==false) {
+			snake.move(2*l+1);
 		}
-		int [][] permute_ary = permute(ary);
-		for(int i=0;i<permute_num(n-1,m-1);i++) {
-			if(i==0) {
-				min_lv = map.path(permute_ary[i]).max_lv();
-			}
-			if(min_lv > map.path(permute_ary[i]).max_lv()) {
-				min_lv = map.path(permute_ary[i]).max_lv();
-			}
-		}
-		System.out.println(min_lv);
+		//System.out.println("end time: " + map.second);
+
+		System.out.println(snake.second);
 		scn.close();
 	}
 }
